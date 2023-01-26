e9774fcc209502cc8f180e2405e72aef36da35c5
Hbourmaud <hugo.bourmaud@ynov.com>
2023-01-26 16:18:19 +0100

    Add QueryToDB func for DB connexion & request

---
+++ ConnectionDB.php
@@ -0,0 +1,17 @@
+<?php
+
+// How To Use:
+// include 'ConnectionDB.php';
+// $result = QueryToDB("SELECT * FROM user");
+// while($row = $result->fetch_assoc()){
+//     print_r($row);
+// }
+
+$mysqli = new mysqli("localhost", "root", "", "php_exam_db");
+function QueryToDB(string $query):mysqli_result
+{
+    global $mysqli;
+    $result = $mysqli->query($query);
+    return $result;
+}
+?>
