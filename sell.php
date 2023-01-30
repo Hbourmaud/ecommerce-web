632404a1b22b0ab2b716b0f835aae0e3fa1c958c
Brante <brandon.porte@ynov.com>
2023-01-30 10:11:02 +0100

    [UPDATE] sell : required, price & number regulation

--- sell.php
+++ sell.php
@@ -5,14 +5,14 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>sell</title>
-	<link></link>
+	<link rel="stylesheet" href="index.css">
 </head>
 <body>
 	<form method="post">
-		Name:  <input type="text" name="username" enctype="multipart/form-data"/><br />
-		Description: <input type="text" name="description" /><br />
-		Price: <input type="text" name="price" /><br />
-		Number: <input type="text" name="number" /><br />
+		Name:  <input type="text" name="username" enctype="multipart/form-data" required="required" /><br />
+		Description: <input type="text" name="description" required="required" /><br />
+		Price: <input type="text" name="price" pattern="\d{1,5}\.[0-9]{2,2}" required="required" /><br />
+		Number: <input type="text" name="number" pattern="\d{1,4}" required="required" /><br />
 		Picture: <input type="file" name="file" /> <input type="submit" name="picture" value="Add picture" /><br />
 		<input type="submit" name="submit" value="Add article" />
 	</form>
@@ -23,4 +23,5 @@ echo $_REQUEST['username'] . "<br />";
 echo $_REQUEST['description'] . "<br />";
 echo $_REQUEST['price'] . "<br />";
 echo $today = date("d/m/Y"); 
+// +\.\d{1,2}
 ?>
