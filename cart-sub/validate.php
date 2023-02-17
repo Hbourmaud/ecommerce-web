f17f6b7893c785361029ba4e837b09928786dbad
Hbourmaud <hugo.bourmaud@ynov.com>
2023-02-17 02:34:10 +0100

    [FEATURE] Generate Invoice with billing info

--- cart-sub/validate.php
+++ cart-sub/validate.php
@@ -9,11 +9,16 @@
 <body>
     <?php
         session_start();
-        if($_SESSION['login'] == "" || $_POST['toPaid'] == ""){
+        include '../common/ConnectionDB.php';
+        if($_SESSION['login'] == "" || ($_POST['toPaid'] == "" && $_POST['addressBilling'] = "")){
             header('Location: ../login');
             Exit();
+        }elseif ($_POST['addressBilling'] != "") {
+            $today = date("d/m/Y");
+            QueryToDB("INSERT INTO invoice (ID_user, `date of a transaction`, amount, billing_adress, billing_city, billing_postal_code) VALUES (\"".$_SESSION['login']."\",\"".$today."\",\"".$_POST['toPaid']."\",\"".$_POST['addressBilling']."\",\"".$_POST['cityBilling']."\",\"".$_POST['postalBilling']."\")");
+            header('Location: ../index');
+            Exit();
         }
-        include '../common/ConnectionDB.php';
         $result = QueryToDB("SELECT balance FROM USER WHERE UUID = \"".$_SESSION['login']."\"");
         while($row = $result->fetch_assoc()){
             $balanceUser = $row['balance'];
@@ -25,6 +30,13 @@
     Total Amount : <?php echo $_POST['toPaid'] ?>
     <br>
     Balance after payment : <?php echo $balanceUser - $_POST['toPaid'] ?>
-    
+    <h3>Billing Information:</h3>
+    <form action="" method="POST">
+        <input type="text" name="addressBilling" placeholder="Billing Address">
+        <input type="text" name="cityBilling" placeholder="Billing City">
+        <input type="number" name="postalBilling" placeholder="Billing Postal Code">
+        <input type="hidden" name="toPaid" value="<?php echo $_POST['toPaid'] ?>">
+        <input type="submit" value="Confirm Payment">
+    </form>
 </body>
 </html>
