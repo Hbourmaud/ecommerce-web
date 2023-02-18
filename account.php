d49032d4f02376b212872bc3ff8463ba37a79596
unknown <brandon.porte@ynov.com>
2023-02-18 03:48:49 +0100

    [FEATURE] account : own account

--- account.php
+++ account.php
@@ -4,9 +4,23 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>sell</title>
+	<title>account</title>
 	<link rel="stylesheet" href="index.css">
 </head>
 <body>
+<?php
+	session_start();
+	include 'common/ConnectionDB.php';
+	if($_SESSION['login'] == ""){
+		header('Location: php_exam/login');
+		Exit();
+	}
 	
+	if(empty($_GET['id'])){     // isset($_GET['Id'])
+		$q = "SELECT * FROM item WHERE id = (SELECT ID FROM user WHERE uuid = '$_SESSION[login]')";
+		echo $q;
+		echo QueryToDB("SELECT * FROM item WHERE id = (SELECT ID FROM user WHERE uuid = '$_SESSION[login]')");
+	}
+?>
 </body>
+</html>
