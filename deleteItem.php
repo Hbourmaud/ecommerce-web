1f20e8cd15cff7248967c8ee04d1170c2d963e72
VBRAVO <valentin.bravo@ynov.com>
2023-02-19 17:40:49 +0100

    [UPDATE] edit : delete from cart and stock

--- deleteItem.php
+++ deleteItem.php
@@ -4,4 +4,6 @@ session_start();
 include 'common/ConnectionDB.php';
 $idItem = $_POST["ArticleId"];
     QueryToDB("DELETE FROM item WHERE ID = ".$idItem."");
+    QueryToDB("DELETE FROM stock WHERE ID_item = ".$idItem."");
+    QueryToDB("DELETE FROM cart WHERE ID_item = ".$idItem."");
     header('Location: index.php');
