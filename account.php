dfb1924615216eaf2ccc303675ce813f8f06ae28
unknown <brandon.porte@ynov.com>
2023-02-19 05:00:59 +0100

    [FEATURE] account : modal for edit

--- account.php
+++ account.php
@@ -4,10 +4,37 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 	<title>account</title>
 	<link rel="stylesheet" href="index.css">
 </head>
 <body>
+
+<!-- Modal -->
+<form method="post">
+     <input type="submit" name="edit" value="edit" />
+</form>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Changing</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+if(array_key_exists('edit', $_POST)){
+    ?>
+    <script> 
+        const ModalOK = new bootstrap.Modal(document.getElementById('exampleModal'));
+        ModalOK.show();
+    </script>
+    <?php
+}
+?>
 <?php
 	session_start();
 	include 'common/ConnectionDB.php';
